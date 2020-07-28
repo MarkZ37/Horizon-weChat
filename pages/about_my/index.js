4acf2bac62820c2cf3e67f511ac4dbb5ff8da08d
markz37 <595223036@qq.com>
2020-07-28 15:35:20 +0800

    成功获取用户信息

--- pages/about_my/index.js
+++ pages/about_my/index.js
@@ -31,48 +31,75 @@ Page({
   dealLogin: function(){
     //授权登录
     wx.login({
-      success: res => {
+      success: function(res) {
         console.log(res)
-		//请求后端换取openid的接口
-        wx.request({
-          url: 'http://localhost:8080/api/user/login',
-          method: 'POST',
-          data: {
-          //将code传到后端
-            code: res.code
-          },
-          success: res => {
-            //获取到openid作为账号密码
-            console.log(res)
-            // console.log(app.globalData.userInfo)
+        wx.getUserInfo({
+          //获取用户信息
+          lang: 'zh_CN',
+          success: function(result) {
+            console.log(result);
             wx.request({
-              url: '/wx-login/',
+              url: 'http://localhost:8080/api/user/login',
               method: 'POST',
               data: {
-                openid: res.openid,
-                session_key: res.session_key,
-                // nickname: app.globalData.userInfo.nickName,
-                // avatar_url: app.globalData.userInfo.avatarUrl,
-                // gender: app.globalData.userInfo.gender
+                encryptedData: result.encryptedData,
+                iv: result.iv,
+                code: res.code
               },
-              //登录成功后返回token保存在storage中
-              success: res => {
-                console.log(res)
-                //token存入storage
-                wx.setStorageSync('jwt_token', res.token)
-                wx.setStorageSync('user_id', res.user_id)
-                this.reFreshUserProfile()
-                //登录状态置为true
-                this.setData({
-                  isLogin: true,
-                  hasUserInfo: true
-                })
-                // app.globalData.isLogin = true
+              success: function(userInfo) {
+                console.log(userInfo);
               }
             })
-
           }
         })
+        // wx.request({
+        //   url: 'http://localhost:8080/api/user/login',
+        //   method: 'POST',
+        //   data: {
+        //   //将code传到后端
+        //     code: res.code,
+        //     // iv: res.iv,
+        //     // encryptedData: res.encryptedData
+        //   },
+        //   success: res => {
+        //     //获取到openid
+        //     console.log(res);
+        //     wx.getUserInfo({
+        //       //获取用户信息，成功后传入后端进行数据解析
+        //       lang: 'zh_CN',
+        //       success: function(r){
+        //         console.log(r);
+        //       }
+        //     })
+        //     // console.log("userInfo:"+app.globalData.userInfo)
+        //     // wx.request({
+        //     //   url: '/wx-login/',
+        //     //   method: 'POST',
+        //     //   data: {
+        //     //     openid: res.openid,
+        //     //     session_key: res.session_key,
+        //     //     // nickname: app.globalData.userInfo.nickName,
+        //     //     // avatar_url: app.globalData.userInfo.avatarUrl,
+        //     //     // gender: app.globalData.userInfo.gender
+        //     //   },
+        //     //   //登录成功后返回token保存在storage中
+        //     //   success: res => {
+        //     //     console.log(res)
+        //     //     //token存入storage
+        //     //     wx.setStorageSync('jwt_token', res.token)
+        //     //     wx.setStorageSync('user_id', res.user_id)
+        //     //     this.reFreshUserProfile()
+        //     //     //登录状态置为true
+        //     //     this.setData({
+        //     //       isLogin: true,
+        //     //       hasUserInfo: true
+        //     //     })
+        //     //     // app.globalData.isLogin = true
+        //     //   }
+        //     // })
+
+        //   }
+        // })
       }
     })
   }
